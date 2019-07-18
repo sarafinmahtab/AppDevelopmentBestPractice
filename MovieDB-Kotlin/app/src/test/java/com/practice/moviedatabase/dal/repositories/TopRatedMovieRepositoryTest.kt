0315c4a5856d9e059d6911630b1cf76ddbebf38f
mahtab <mahtab@m2sys.com>
2019-07-18 15:53:47 +0600

    TopRatedMovie Unit Test updated

--- MovieDB-Kotlin/app/src/test/java/com/practice/moviedatabase/dal/repositories/TopRatedMovieRepositoryTest.kt
+++ MovieDB-Kotlin/app/src/test/java/com/practice/moviedatabase/dal/repositories/TopRatedMovieRepositoryTest.kt
@@ -1,15 +1,18 @@
 package com.practice.moviedatabase.dal.repositories
 
+import android.accounts.NetworkErrorException
 import com.practice.moviedatabase.bll.TopRatedMovieUseCase
 import com.practice.moviedatabase.dal.networks.ServerConstants
 import com.practice.moviedatabase.models.Movie
 import com.practice.moviedatabase.models.Result
 import com.practice.moviedatabase.models.params.TopRatedMovieParams
 import io.mockk.MockKAnnotations
-import io.mockk.every
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.instanceOf
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
@@ -19,6 +22,9 @@ class TopRatedMovieRepositoryTest {
     private val useCaseMock = mockk<TopRatedMovieUseCase>()
     private val SUT: TopRatedMovieRepository = TopRatedMovieRepository(useCaseMock)
 
+    private val params =
+        TopRatedMovieParams(ServerConstants.apiKey, ServerConstants.language, "1", ServerConstants.sortedBy)
+
     @Before
     fun setUp() {
         MockKAnnotations.init(this, relaxUnitFun = true)
@@ -26,31 +32,70 @@ class TopRatedMovieRepositoryTest {
     }
 
     @Test
+    fun fetchTopRatedMovie_correctParameterPassed() {
+
+        runBlocking {
+            SUT.fetchTopRatedMovies(params)
+        }
+
+        coVerify (exactly = 1) {
+            useCaseMock.invoke(params)
+        }
+    }
+
+    @Test
     fun fetchTopRatedMovie_success_successReturned() {
 
         val result = runBlocking {
-            SUT.fetchTopRatedMovies(
-                TopRatedMovieParams(ServerConstants.apiKey, ServerConstants.language, "1", ServerConstants.sortedBy)
-            )
+            SUT.fetchTopRatedMovies(params)
         }
 
         assertThat(result.status, `is`(Result.Status.SUCCESS))
+        assertThat(result.data, `is`(instanceOf(List::class.java)))
+    }
+
+    @Test
+    fun fetchTopRatedMovie_generalError_errorReturned() {
+
+        generalError()
+
+        val result = runBlocking {
+            SUT.fetchTopRatedMovies(params)
+        }
+
+        assertThat(result.status, `is`(Result.Status.ERROR))
+    }
+
+    @Test
+    fun fetchTopRatedMovie_networkError_errorReturned() {
+
+        networkError()
+
+        val result = runBlocking {
+            SUT.fetchTopRatedMovies(params)
+        }
+
+        assertThat(result.status, `is`(Result.Status.ERROR))
+        assertThat(result.error, `is`(instanceOf(NetworkErrorException::class.java)))
+    }
+
+
+    private fun networkError() {
+        coEvery {
+            useCaseMock(params)
+        } returns Result.error(NetworkErrorException("Network Error Exception caught"))
+    }
+
+    private fun generalError() {
+        coEvery {
+            useCaseMock(params)
+        } returns Result.error(Exception("Exception caught"))
     }
 
     private fun successStubbing() {
 
-        every {
-
-            runBlocking {
-                useCaseMock(
-                    TopRatedMovieParams(
-                        ServerConstants.apiKey,
-                        ServerConstants.language,
-                        "1",
-                        ServerConstants.sortedBy
-                    )
-                )
-            }
+        coEvery {
+            useCaseMock(params)
         } returns Result.success(
             listOf(
                 Movie(
